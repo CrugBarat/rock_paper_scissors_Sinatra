4c2f180a18040a4137c2e8e0f7582d7cb4232dc7
Craig Barratt <craigtbarratt@gmail.com>
2020-03-31 01:51:49 +0100

    adds css styling

--- app.rb
+++ app.rb
@@ -3,12 +3,15 @@ require('sinatra/contrib/all') if development?
 
 require_relative('./models/game.rb')
 
-get '/rps' do
+get '/rps-home' do
   erb(:home)
 end
 
+get '/rps-home/play' do
+  erb(:play)
+end
 
-get '/rps/:player1/:player2' do
+get '/rps-home/play/:player1/:player2' do
   player1 = params['player1']
   player2 = params['player2']
   @result = Game.run(player1, player2)
